b988e7ba27f8327c94223a60e3a1c26f2a332c05
Triglyceride <jamesmatsu@gmail.com>
2023-01-25 16:08:58 -0800

    fixed typeDef to match client and models

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,25 +2,47 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID!
-    username: String!
-    email: String!
-    password: String!
+    _id: ID
+    username: String
+    email: String
+    password: String
     savedBooks: [Book]
   }
 
   type Book {
-    _id: ID!
+    _id: ID
     authors:[String]
-    description: String!
-    bookId: String!
+    description: String
+    bookId: String
     image: String
     link: String
-    title: String!
+    title: String
+  }
+
+  input savedBook {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth{
+    token: ID!
+    user: User
   }
 
   type Query {
-    user: [user]
-
+    getMe: User
   }
-`
+
+  type Mutation {
+    login(email: String!, password: String! ): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
